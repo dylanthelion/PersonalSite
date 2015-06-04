27dfc89fdfd6a5983abe8f5fcaf737578db5fe04
dylanthelion <dillion256@gmail.com>
2015-06-04 11:37:56 -0500

    Done

---
+++ PersonalSite/Helpers/GlobalVars.cs
@@ -0,0 +1,32 @@
+﻿using PersonalSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalSite.Helpers
+{
+    public static class GlobalVars
+    {
+
+        public static SelectList LanguageChoices
+        {
+            get
+            {
+                return new SelectList(
+                    new List<SelectListItem>
+                    {
+                        new SelectListItem { Text = "Please Choose a language", Value = "Please Choose a Language" },
+                        new SelectListItem { Text = "C#", Value = ((int)Language.CSharp).ToString() },
+                        new SelectListItem { Text = "ObjC", Value = ((int)Language.ObjC).ToString() },
+                        new SelectListItem { Text = "Swift", Value = ((int)Language.Swift).ToString() }
+                    }, "Value", "Text"
+                );
+            }
+            
+        }
+
+        
+    }
+}
--- PersonalSite/Models/Snippet.cs
+++ PersonalSite/Models/Snippet.cs
@@ -42,4 +42,6 @@ namespace PersonalSite.Models
         ObjC,
         Swift
     };
+
+    
 }
